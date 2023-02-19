b48fdee9816d5eb811cd61c18e1b054e60b6d804
lagent_titi <123580047+lagentiti@users.noreply.github.com>
2023-02-19 15:43:35 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# iptv_player
+# IPTV PLAYER
+
+## To Use
+
+To clone and run this repository you'll need [Git](https://git-scm.com) and [Node.js](https://nodejs.org/en/download/) (which comes with [npm](http://npmjs.com)) installed on your computer. From your command line:
+
+```bash
+# Clone this repository
+git clone https://github.com/lagentiti/iptv_player
+# Go into the repository
+cd iptv_player
+# Install dependencies
+npm install
+# Run the app
+npm start
+```
+
+## License
+
+[MIT](LICENSE.md)
---
+++ preload.js
@@ -0,0 +1,23 @@
+/**
+ * The preload script runs before. It has access to web APIs
+ * as well as Electron's renderer process modules and some
+ * polyfilled Node.js functions.
+ * 
+ * https://www.electronjs.org/docs/latest/tutorial/sandbox
+ */
+const customTitlebar = require('custom-electron-titlebar');
+
+window.addEventListener('DOMContentLoaded', () => {
+  new customTitlebar.Titlebar({
+    backgroundColor: customTitlebar.Color.fromHex('#2f3241')
+  });
+
+  const replaceText = (selector, text) => {
+    const element = document.getElementById(selector)
+    if (element) element.innerText = text
+  }
+
+  for (const type of ['chrome', 'node', 'electron']) {
+    replaceText(`${type}-version`, process.versions[type])
+  }
+})
